56f1e4b7dcbae307d186a54a3023811fc309794d
Ian Abreu <ianabreudev@gmail.com>
2025-02-12 22:21:21 -0300

    add options cors

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://my-tickets-rho.vercel.app"
+    "https://my-tickets-web.vercel.app"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
